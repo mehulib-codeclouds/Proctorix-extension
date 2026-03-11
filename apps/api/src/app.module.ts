303a7f02aee9caaf357d0b9e2c0c9fc1c303257d
Arjo Ghosh <arjo.ghosh@codeclouds.co.in>
2026-03-11 09:32:30 +0000

    feat(exams): implement create exam service

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -1,5 +1,7 @@
 import { ConfigifyModule } from '@itgorillaz/configify';
+import { ApolloDriver, type ApolloDriverConfig } from '@nestjs/apollo';
 import { Module } from '@nestjs/common';
+import { GraphQLModule } from '@nestjs/graphql';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppConfiguration } from './config/app.config';
 import { Attempt } from './entities/attempt.entity';
@@ -13,6 +15,8 @@ import { MsqOption } from './entities/msq-option.entity';
 import { Question } from './entities/question.entity';
 import { Session } from './entities/session.entity';
 import { User } from './entities/user.entity';
+import {  ExamsModule } from './exam/exams.module';
+import { GqlModule } from './gql/exams/gql.module';
 
 @Module({
   imports: [
@@ -37,6 +41,19 @@ import { User } from './entities/user.entity';
       }),
       inject: [AppConfiguration],
     }),
+    GraphQLModule.forRootAsync<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      useFactory: (appConfiguration: AppConfiguration) => ({
+        playground: appConfiguration.nodeEnv === 'dev',
+        autoSchemaFile: true,
+        
+      }),
+      inject: [AppConfiguration],
+    }),
+    ExamsModule,
+    GqlModule,
+
+
   ],
 })
 export class AppModule {}
