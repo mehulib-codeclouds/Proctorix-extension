151d57e89728f9595b8a77a702d5d7484f997e8e
Sanchita76 <cse2022022@rcciit.org.in>
2026-04-08 16:48:44 +0530

    add all files

--- apps/web/run_extensions.ts
+++ apps/web/run_extensions.ts
@@ -1,1 +1,5 @@
 window.postMessage({ type: 'PROCTORIX_START' }, '*');
+if (/Mobi|Android|iPhone|iPad/i.test(navigator.userAgent)) {
+  // Redirect to a "desktop required" page
+  window.location.href = '/desktop-required';
+}
